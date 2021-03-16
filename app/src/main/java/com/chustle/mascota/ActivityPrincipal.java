b93dea92a77833b5ff81f4fd75b10ee95cc6a95e
Juan Daniel Rodríguez Espinoza <drany@live.com>
2021-03-16 06:06:19 -0600

    Proyecto creado

---
+++ app/src/main/java/com/chustle/mascota/ActivityPrincipal.java
@@ -0,0 +1,14 @@
+package com.chustle.mascota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
