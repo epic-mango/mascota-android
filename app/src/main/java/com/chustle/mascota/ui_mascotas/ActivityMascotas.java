ec13e8a997bec7dd1cbb23ce5ab0531fd14c07c8
Juan Daniel Rodríguez Espinoza <drany@live.com>
2021-05-25 20:06:12 -0500

    Eliminar mascotas listo

--- app/src/main/java/com/chustle/mascota/ui_mascotas/ActivityMascotas.java
+++ app/src/main/java/com/chustle/mascota/ui_mascotas/ActivityMascotas.java
@@ -1,6 +1,7 @@
 package com.chustle.mascota.ui_mascotas;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -35,7 +37,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -168,7 +169,70 @@ public class ActivityMascotas extends AppCompatActivity {
 
             @Override
             public void onLongClickListener(int position) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(ActivityMascotas.this);
+                alert.setMessage(getResources().getString(R.string.advertencia_eliminar));
+                alert.setTitle(getResources().getString(R.string.eliminar));
+                alert.setPositiveButton(getString(R.string.aceptar), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        RequestQueue cola = Volley.newRequestQueue(getApplicationContext());
+                        String URL = Uri.parse(Config.URL + "mascotas.php")
+                                .buildUpon()
+                                .appendQueryParameter("id", Integer.toString(mascotas.get(position).id))
+                                .build().toString();
 
+
+                        StringRequest peticion = new StringRequest(Request.Method.DELETE,
+                                URL,
+                                new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String response) {
+                                        try {
+                                            Log.i("Response", response);
+                                            JSONObject json = new JSONObject(response);
+                                            String estado = json.getString("estado");
+
+                                            if (estado.equals("true")) {
+                                                mascotas.remove(position);
+                                                rvMascotas.getAdapter().notifyItemRemoved(position);
+                                            } else {
+                                                Toast.makeText(ActivityMascotas.this, getString(R.string.error_eliminar), Toast.LENGTH_SHORT).show();
+                                            }
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Toast.makeText(ActivityMascotas.this, getString(R.string.error_red), Toast.LENGTH_SHORT).show();
+                                        Log.e("", "onErrorResponse: ", error.getCause());
+                                    }
+                                }) {
+
+                            @Override
+                            public Map<String, String> getHeaders() throws AuthFailureError {
+                                Map<String, String> headers = new HashMap<>();
+
+                                headers.put("Authorization", getSharedPreferences("cuenta", Context.MODE_PRIVATE).getString("token", ""));
+
+                                return headers;
+                            }
+                        };
+
+                        peticion.setRetryPolicy(new DefaultRetryPolicy(3600, 0, 0));
+                        cola.add(peticion);
+                    }
+                });
+
+                alert.setNegativeButton(getString(R.string.cancelar), null);
+
+                alert.create();
+                alert.show();
             }
         }));
     }
